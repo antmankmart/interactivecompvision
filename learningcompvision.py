3ada34fec27bfcf36d1a1ff871a6d11a43915d53
Anitya <anityakumarssj02@gmail.com>
2024-05-09 12:46:08 -0400

    added stuff to test camera function

--- learningcompvision.py
+++ learningcompvision.py
@@ -5,6 +5,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from zipfile import ZipFile
 from urllib.request import urlretrieve
+import sys
 
 from IPython.display import Image
 
@@ -36,3 +37,22 @@ if not os.path.exists(asset_zip_path):
 
 
 Image(filename = "checkerboard_18x18.png")
+
+
+s = 0
+if len(sys.argv) > 1:
+    s = sys.argv[1]
+
+source = cv2.VideoCapture(s)
+
+win_name = 'Camera Preview'
+cv2.namedWindow(win_name, cv2.WINDOW_NORMAL)
+
+while cv2.waitKey(1) != 27: # Escape
+    has_frame, frame = source.read()
+    if not has_frame:
+        break
+    cv2.imshow(win_name, frame)
+
+source.release()
+cv2.destroyWindow(win_name)
